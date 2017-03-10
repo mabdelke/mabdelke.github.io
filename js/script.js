6db9d473892af031b7e538a8c56f121e3c53cd49
mabdelke <mht15@live.fr>
2017-03-10 19:26:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function fonctionRequeteApi (url,id)
-{var xmlhttp = new XMLHttpRequest();
+{
+    var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
@@ -24,11 +25,5 @@ function homePageLoading() {
   url = 'http://bitcoin.mubiz.com/mininginfo';
     elementID = 'mininginfo';
     fonctionRequeteApi(url, elementID);
-  url = 'http://bitcoin.mubiz.com/help';
-    elementID = 'help';
-    fonctionRequeteApi(url, elementID);
-  
-
-
 }
 
